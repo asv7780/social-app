b8b2dac806decea6f84ff31e3e42745774ee3dfc
asv7780 <asv7780@gmail.com>
2019-10-08 11:21:03 +0300

    Updates

---
+++ precache-manifest.dab5b2a965a322cac902b14bc6f9b1fd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4803873193ce2ce2bd9854d021d7adca",
+    "url": "/social-app/index.html"
+  },
+  {
+    "revision": "ee3563f6350a1633967f",
+    "url": "/social-app/static/css/3.2a63f19b.chunk.css"
+  },
+  {
+    "revision": "ef1b3ad52e027812a601",
+    "url": "/social-app/static/css/4.f8a43f11.chunk.css"
+  },
+  {
+    "revision": "08bbdb5ddea845e5a0d7",
+    "url": "/social-app/static/css/main.7fe32ff2.chunk.css"
+  },
+  {
+    "revision": "59113a1bec0c03518ce1",
+    "url": "/social-app/static/js/2.60103322.chunk.js"
+  },
+  {
+    "revision": "ee3563f6350a1633967f",
+    "url": "/social-app/static/js/3.f34f2be7.chunk.js"
+  },
+  {
+    "revision": "ef1b3ad52e027812a601",
+    "url": "/social-app/static/js/4.270269ad.chunk.js"
+  },
+  {
+    "revision": "08bbdb5ddea845e5a0d7",
+    "url": "/social-app/static/js/main.3970b488.chunk.js"
+  },
+  {
+    "revision": "a7fc174471ca6573c13c",
+    "url": "/social-app/static/js/runtime~main.1bd87c8a.js"
+  },
+  {
+    "revision": "5088b6367fb81e33ce03ee562a289dd6",
+    "url": "/social-app/static/media/Spinner-1.3s-200px.5088b636.svg"
+  },
+  {
+    "revision": "f07d66e00150e23a0d8b2e10b7b0f86b",
+    "url": "/social-app/static/media/user-logo.f07d66e0.png"
+  }
+]);
